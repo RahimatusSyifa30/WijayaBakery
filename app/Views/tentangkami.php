f7488025cd56193028f4129b86fb45a2a8aa743b
RahimatusSyifa30 <sifaspt317@gmail.com>
2023-05-23 16:47:53 +0700

    Page Tentang Kami Fix

--- app/Views/tentangkami.php
+++ app/Views/tentangkami.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Wijaya Bakery - Beranda</title>
+    <title>Wijaya Bakery - Tentang Kami</title>
     <link rel="shortcut icon" href="image/logo/logo.png" />
-    <!-- Bootstrap -->
+  
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
       rel="stylesheet"
@@ -14,24 +14,72 @@
       crossorigin="anonymous"
     />
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
       crossorigin="anonymous"
     ></script>
     <link rel="stylesheet" href="<?= base_url('css/basic.css')?>">
+    <link rel="stylesheet" href="<?= base_url('css/tentangkami.css')?>" />
    
   </head>
   <body>
     <!-- Header Start -->
     <?php include("layout/header.php") ?>
     <!-- Header End -->
-    <section id="tentang">
-      <div class="tentang">
-
+    
+    <section id="about">
+      <div class="about-1">
+        <h1>Tentang Kami</h1>
+        <img
+                src="image/logo/logo.png"
+                class="rounded mx-auto d-block"
+                alt="..."
+              />
+        <p>Toko Roti Wijaya Bakery adalah sebuah perusahaan toko roti yang didirikan pada tahun 2020 dengan tujuan menyediakan roti yang berkualitas kepada pelanggan. 
+          Beralamatkan di Jl. Raya Pakuniran, RT.16/RW.04, Kolor, Bucor Kulon, Kec. Pakuniran, Kabupaten Probolinggo, Jawa Timur 67292. 
+          Wijaya Bakery menyajikan roti lezat dan inovatif yang memuaskan selera pelanggan. </p>
+          <p>Wijaya Bakery memiliki berbagai macam pilihan roti, seperti brownis, donat, kue tart, pizza, dan roti.</p>
+          <hr>
       </div>
-
-    </section>
+      <div id="about-2">
+        <div class="content-box-lg">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-4">
+                <div class="about-item text-center">
+                  <i class="fa fa-globe"></i>
+                  <h3> Visi </h3>
+                  <hr>
+                  <p>Menjadi toko roti yang dikenal secara luas karena inovasi, kualitas, dan keunikan produk roti yang kami tawarkan kepada pelanggan.</p>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="about-item text-center">
+                  <i class="fa fa-book"></i>
+                  <h3> Misi </h3>
+                  <hr>
+                  <p>1.	Menyediakan roti berkualitas tinggi dan beragam dengan rasa yang unik dan menarik bagi pelanggan kami.</p>
+                  <p>2.	Membangun hubungan yang kokoh dengan pelanggan kami dengan memberikan pelayanan yang ramah, responsif, dan profesional.</p>
+                  <p>3.	Menjaga Kualitas dan Kebersihan: Kebersihan dan keamanan adalah prioritas utama kami. Kami menjaga kebersihan ruang produksi, peralatan, dan bahan baku dengan standar yang tinggi.</p>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="about-item text-center">
+                  <i class="fa fa-handshake-o"></i>
+                  <h3> Tujuan </h3>
+                  <hr>
+                  <p>1. Memperluas jangkauan pasar dan meningkatkan kesadaran merek kami melalui strategi pemasaran yang efektif,termasuk penggunaan media sosial.</p>
+                  <p>2.	Menjadi tempat yang diandalkan untuk kebutuhan roti sehari-hari pelanggan dengan menyediakan berbagai macam pilihan roti.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>   
     
     <!-- Footer Start -->
     <?php include("layout/footer.php") ?>
